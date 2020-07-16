3e364555287dc04ad1502bf48b04e79cc0e1085a
SamantaGomez <samygomezjacome@gmail.com>
2020-07-15 21:10:01 -0500

    #15 relaciones

---
+++ app/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Comment extends Model
+{
+    protected $fillable = ['text'];
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($article)
+        {
+            $article->user_id = Auth::id();
+        });
+    }
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function article()
+    {
+        return $this->belongsTo('App\Article');
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Category;
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Vaciar la tabla
+        User::truncate();
+
+        $faker = \Faker\Factory::create();
+        $password =Hash::make('123123');
+        User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@prueba.com',
+            'password' => $password,
+        ]);
+
+        for ($i = 0; $i < 50; $i++) {
+            $user=User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
+            ]);
+            $user->categories()->saveMany(
+                $faker->randomElements(
+                    array(
+                        Category::find(1),
+                        Category::find(2),
+                        Category::find(3)
+                    ),
+                        $faker->numberBetween(1, 3), false
+                )
+            );
+        }
+
+    }
+}
